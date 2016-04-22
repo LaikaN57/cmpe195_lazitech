77b1fa1b86330b6893323793c1ff5fe6b6c6c837
yasminejamjoum <yasminejamjoum@gmail.com>
2016-04-22 16:30:19 -0700

    Update test_pdf_gfx.h

--- software/test_pdf_gfx.h
+++ software/test_pdf_gfx.h
@@ -13,13 +13,25 @@ enum color_space_t {
 };
 
 enum line_cap_t {
-	square_butt_caps = 0,
-	
+	square_butt_caps = 0, // ends of path squared off without projection
+	square_projecting_caps = 1, // ends of path squared off with projection
+	round_caps = 2 // ends of path circled off 
 };
 
 enum line_join_t {
-	mitered_joins = 0,
+	mitered_joins = 0, // angled edge (sharp edge)
+	rounded_joins = 1, // rounded edge
+	bevel_joins = 2 // butt caps
 	
+};
+
+enum dash_pattern_t {
+	no_dash = 0, //no dash; solid 	
+	equal_dash = 1, // 3 units off, 3 units on
+	dash_pattern_a = 2; // 1 unit on, 2 units off
+	dash_pattern_b = 3; // 2 units on, 1 unit off
+	dash_pattern_c = 4; // 2 units off, 3 units on, 5 units off
+	dash_pattern_d = 5; // 1 unit on, 3 units off, 2 units on
 };
 
 // graphics state
@@ -33,6 +45,7 @@ typedef struct {
 	line_cap_t line_cap = square_butt_caps,
 	line_join_t line_join = mitered_joins,
 	double miter_limit = 10.0,
-	void * dash_pattern,
+	//void * dash_pattern,
+	dash_pattern_t dash_pattern = no_dash,
 	int stroke_adjustment = 0,
 } gfx_state_t;
