67c03720382389c32805cf7c6f481b42401c7d2a
yasminejamjoum <yasminejamjoum@gmail.com>
2016-08-31 11:07:30 -0700

    Update module.c

--- software/module_utils/module.c
+++ software/module_utils/module.c
@@ -2,7 +2,7 @@
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Irlanda A <>");
-MODULE_AUTHOR("Yasmine J <>");
+MODULE_AUTHOR("Yasmine Jamjoum <yasminejamjoum@gmail.com>");
 MODULE_AUTHOR("Alex Kennedy <alexzanderkennedy@gmail.com>");
 MODULE_DESCRIPTION("Module");
 MODULE_VERSION("0:1.0.1");
@@ -17,9 +17,14 @@ int __init_or_module my_module_init(void) {
 	return 0;
 }
 
+void __cut my_module_cut(void) {
+	
+}
+
 void __exit my_module_exit() {
 }
 
 module_init(my_module_init);
+module_cut(my_module_cut);
 module_exit(my_module_exit);
 
