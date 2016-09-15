fb53d3bb14de3e45b0d241bac4f59ed7fff34fd6
yasminejamjoum <yasminejamjoum@gmail.com>
2016-09-15 16:59:43 -0700

    Update LazTech.cpp

--- software/LazTech.cpp
+++ software/LazTech.cpp
@@ -14,22 +14,66 @@ LazTech::LazTech(double x, double y)
 {
 	x1 = x;
 	y1 = y;
+	x2 = 0;
+	y2 = 0;
+	x3 = 0;
+	y3 = 0;
+	x4 = 0;
+	y4 = 0;
+	stroke = 0;
+	fill = 0;
+	width = 0;
+	scale = 0;
+	velocity = 0;
+	input = 0;
 }
 
 LazTech::LazTech()
 {
 	x1 = 0;
 	y1 = 0;
+	x2 = 0;
+	y2 = 0;
+	x3 = 0;
+	y3 = 0;
+	x4 = 0;
+	y4 = 0;
+	stroke = 0;
+	fill = 0;
+	width = 0;
+	scale = 0;
+	velocity = 0;
+	input = 0;
 }
 
 void LazTech::start()
 {
+	delay(1); //start with a second delay
 
+	//steps per revolution
+
+	STEP_ON; //STEP set to 1
+	DIR_ON; //DIR set to 1
+	MS1_ON; //MS1 set to 1
+	MS2_ON; //MS2 set to 1
+	EN_ON; //EN set to 1
+	RESET_OFF; // RESET set to 0
+	cout << "Enabling Stepper Motor";
+
+	//start the velocity
 }
 
 void LazTech::end()
 {
+	STEP_OFF; // STEP set to 0
+	DIR_OFF; //DIR set to 0
+	MS1_OFF; //MS1 set to 0
+	MS2_OFF; //MS2 set to 0
+	EN_OFF; // EN set to 1
+	RESET_OFF; //RESET set to 1
 
+	delay(1);//end with a second delay
+	cout << "Disabling Stepper Motor";
 }
 
 double LazTech::get_x()
@@ -60,24 +104,40 @@ void LazTech::origin (double x, double y)
 	double yOffset =  y - height; // actual y - default
 }
 
-void LazTech::cutLine()
+void LazTech::cutLine(double x1, double y1, double x2, double y2, double x3, double y3, double x4, double y4) // input as an XYXYXYXY
 {
+	start();
 
+	input[8] = {  x1,  y1,  x2,  y2,  x3,  y3,  x4,  y4 }; // map the 8 pts as input
+	double mid_x = ((x1 + x2)/2);
+	double mid_y = ((y1 + y2)/2);
+
+	for (int i = 0; i < step ; i++){
+		STEP_ON;
+		delay(0.5);
+		STEP_OFF;
+		delay(0.5);
+	}
+
+	end();
 }
 
 void LazTech::cutCurve()
 {
-
+	start();
+	cutLine();
+	bezierCurve( x1,  y1,  x2,  y2,  x3,  y3,  x4,  y4);
+	end();
 }
 
 void LazTech::defaultState(string stroke, double width, double start)
 {
 	stroke = "black";
 	width = 1;
-	start = LazTech(0,0);
+	start = origin(0, 0);
 }
 
-//Bezier Curve formula
+//Bezier Curve formula calculates pt on curve with 4 controlpts
 double LazTech::bezierCurve (double x1, double y1, double x2, double y2, double x3, double y3, double x4, double y4)
 {
 	double Pt1 = ( x1,  y1);
@@ -91,7 +151,9 @@ double LazTech::bezierCurve (double x1, double y1, double x2, double y2, double 
 	int tx = 1-t;
 	int tx2 = tx * tx;
 	int tx3 = tx2 * tx;
-	return ((tx3 * Pt1) + (3 * t * tx2 * Pt2) + (3 * t2 * tx * Pt3) + (t3 * Pt4));
+	int pt ((tx3 * Pt1) + (3 * t * tx2 * Pt2) + (3 * t2 * tx * Pt3) + (t3 * Pt4));
+
+	return pt;
 
 }
 
@@ -104,4 +166,3 @@ void LazTech::stroke_line()
 {
 
 }
-
