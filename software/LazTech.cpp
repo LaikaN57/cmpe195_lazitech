37c70a0e4bab54dde502f1334f92c63fe428a9dd
yasminejamjoum <yasminejamjoum@gmail.com>
2016-10-02 00:19:04 -0700

    Update LazTech.cpp

--- software/LazTech.cpp
+++ software/LazTech.cpp
@@ -1,13 +1,13 @@
-//============================================================================
-// Name        : LazTech.cpp
-// Author      : 
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
-//============================================================================
-
 #include <iostream>
+#include <string>
+#include <memory>
+#include <vector>
+#include <stdlib.h>
+#include <cmath>
+
 #include "LazTech.h"
+#include "Pt.cpp"
+
 using namespace std;
 
 LazTech::LazTech(double x, double y)
@@ -25,7 +25,7 @@ LazTech::LazTech(double x, double y)
 	width = 0;
 	scale = 0;
 	velocity = 0;
-	input = 0;
+	//input = 0;
 }
 
 LazTech::LazTech()
@@ -43,9 +43,12 @@ LazTech::LazTech()
 	width = 0;
 	scale = 0;
 	velocity = 0;
-	input = 0;
+	//input = 0;
 }
 
+LazTech:: ~LazTech(){
+
+}
 void LazTech::start()
 {
 	delay(1); //start with a second delay
@@ -75,6 +78,16 @@ void LazTech::end()
 	delay(1);//end with a second delay
 	cout << "Disabling Stepper Motor";
 }
+
+void LazTech::delay(double a)
+{
+	for (int i = 0; i < 1000; i++){
+		for (int j = 0; j < i; j++){
+			//intentionally left blank
+		}
+	}
+}
+
 
 double LazTech::get_x()
 {
@@ -108,7 +121,7 @@ void LazTech::cutLine(double x1, double y1, double x2, double y2, double x3, dou
 {
 	start();
 
-	input[8] = {  x1,  y1,  x2,  y2,  x3,  y3,  x4,  y4 }; // map the 8 pts as input
+	double input[8] = { x1, y1, x2, y2, x3, y3, x4, y4 }; // map the 8 pts as input
 	double mid_x = ((x1 + x2)/2);
 	double mid_y = ((y1 + y2)/2);
 
@@ -122,19 +135,19 @@ void LazTech::cutLine(double x1, double y1, double x2, double y2, double x3, dou
 	end();
 }
 
-void LazTech::cutCurve()
+void LazTech::cutCurve(double x, double y, string stroke)
 {
 	start();
-	cutLine();
+	//cutLine();
 	bezierCurve( x1,  y1,  x2,  y2,  x3,  y3,  x4,  y4);
 	end();
 }
 
-void LazTech::defaultState(string stroke, double width, double start)
+void LazTech::defaultState(string stroke, double width)
 {
 	stroke = "black";
 	width = 1;
-	start = origin(0, 0);
+	origin(0, 0); //start pt
 }
 
 //Bezier Curve formula calculates pt on curve with 4 controlpts
@@ -156,6 +169,82 @@ double LazTech::bezierCurve (double x1, double y1, double x2, double y2, double 
 	return pt;
 
 }
+
+//http://stackoverflow.com/questions/785097/how-do-i-implement-a-bézier-curve-in-c
+double LazTech:: getPt( double n1 , double n2 , float perc )
+{
+    double diff = n2 - n1;
+
+    double result = n1 + ( diff * perc );
+
+    return result;
+}
+
+double LazTech:: factorial(double num)
+{
+	if (num <= 1)
+	{
+		return 1;
+	}
+	else
+	{
+		num = num*factorial(num-1);
+		return num;
+	}
+}
+
+double LazTech:: binomial_coeff(double a, double b)
+{
+	double diff = a - b;
+	double b_diff = factorial(b) * factorial(diff);
+	double coeff = factorial(a) / (b_diff);
+	return coeff;
+
+}
+
+void LazTech:: getPt_2(){
+for( double i = 0 ; i < 1 ; i += 0.01 ){
+    x1 = getPt( x1 , x2 , i );
+    y1 = getPt( y1 , y2 , i );
+    x2 = getPt( x2 , x3 , i );
+    y2 = getPt( y2 , y3 , i );
+
+   double x = getPt( x1 , x2 , i );
+   double y = getPt( y1 , y2 , i );
+
+   cutCurve( x , y , "black" );
+}
+}
+
+//******************************************************************************
+//Curve with multiple points inputed
+
+//Pt LazTech:: getBezierPoint( std::vector<Pt>& pts, double t)
+//{
+//	Pt pt;
+//	//pt.x = 0;
+//	//pt.y = 0;
+//	std::size_t length = pts.size(); //size in bytes
+//
+//	for(std::size_t i = 0; i < length; i++)
+//	{
+//		pt.x = pow((1-t), 3) * pts[0].x + 3 * t * pow((1-t), 2) * pts[1].x + 3 * (1-t) * pow(t, 2) * pts[2].x + pow(t, 3) * pts[3].x;
+//		pt.x = pow((1-t), 3) * pts[0].y + 3 * t * pow((1-t), 2) * pts[1].y + 3 * (1-t) * pow(t, 2) * pts[2].y + pow(t, 3) * pts[3].y;
+//	}
+//
+////	Pt* tmp = new Pt[numPoints];
+////    memcpy(tmp, points, numPoints * sizeof(Pt));
+////    int i = numPoints - 1;
+////    while (i > 0) {
+////        for (int k = 0; k < i; k++)
+////            tmp[k] = tmp[k] + t * ( tmp[k+1] - tmp[k] );
+////        i--;
+////    }
+////    Pt answer = tmp[0];
+////    delete[] tmp;
+//    return pt;
+//};
+//**************************************************************************************
 
 void LazTech::add_curve(int a, int b, int c, int d, int e, int f)
 {
